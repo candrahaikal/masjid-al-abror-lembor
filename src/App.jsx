11908af06f13ee92150698ac89f42cbd06e7eae5
candra haikal <achkennys@gmail.com>
2024-04-11 09:20:08 +0700

    change element beranda to index

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <BrowserRouter >
       <Routes>
-        <Route path="/" element={<Beranda />}/>
+        <Route path="/" index={<Beranda />}/>
         <Route path="profil" element={<Profil />}/>
         <Route path="kontak" element={<Kontak />}/>
         <Route path="donasi" element={<Donasi />}/>
